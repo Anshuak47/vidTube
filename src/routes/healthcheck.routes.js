73258dd74b2c12555450ac9c2f555c65d7abf76c
Anshu <kushwaha.anshu8@gmail.com>
2025-08-06 10:03:29 +0530

    File upload functionality added

--- src/routes/healthcheck.routes.js
+++ src/routes/healthcheck.routes.js
@@ -1,5 +1,7 @@
 import { Router } from "express";
 import { healthcheck } from "../controllers/healthCheckControllers.js";
+import { upload } from "../middlewares/multer.middlewares.js";
+
 const router = Router();
 router.route("/").get(healthcheck);
 export default router;
---
+++ src/utils/cloudinary.js
@@ -0,0 +1,26 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+//CONFIGURE CLOUDINARY
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_CLOUD_KEY,
+  api_secret: process.env.CLOUDINARY_CLOUD_SECRET, // Click 'View API Keys' above to copy your API secret
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+
+    console.log("File uploaded successfully" + response.url);
+
+    // After upload delete from server
+    fs.unlinkSync(localFilePath);
+    return response;
+  } catch (error) {
+    fs.unlinkSync(localFilePath);
+    return null;
+  }
+};
